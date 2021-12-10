908d4fe9ac52652115e194b62d36f77be14fd94e
Vladislav Mamon <norskeld@gmail.com>
2021-12-11 04:19:17 +0500

    feat: add security headers

---
+++ next.config.mjs
@@ -0,0 +1,58 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com;
+  child-src *.youtube.com *.youtube-nocookie.com;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`
+
+const headers = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, '')
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()'
+  }
+]
+
+export default {
+  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers
+      },
+      {
+        source: '/:path*',
+        headers
+      }
+    ]
+  }
+}
