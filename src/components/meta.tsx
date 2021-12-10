d5cb972e8825dfbba3f4c00adc95f36eb6958dff
Vladislav Mamon <norskeld@gmail.com>
2021-12-10 17:43:18 +0500

    fix: use correct favicons' paths

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -5,11 +5,11 @@ import { metadata } from '@/metadata'
 export default function Meta() {
   return (
     <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
 
       <meta name="theme-color" content="#ffffff" />
       <meta name="author" content={metadata.author} />
