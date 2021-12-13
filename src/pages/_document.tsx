1c2c8f96d7a86508665a48367afbbfb2685d0970
Vladislav Mamon <norskeld@gmail.com>
2021-12-13 12:04:58 +0500

    fix: define `og:` namespace for Open Graph

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class VmDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" prefix="og: http://ogp.me/ns#">
         <Head />
         <body>
           <Main />
