67ae7b1da3174cd636541101768e001d1f3bf4d8
Vladislav Mamon <norskeld@gmail.com>
2022-06-12 18:30:40 +0500

    feat: add sitemap.xml and robots.txt generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://vm.codes',
+  changefreq: 'weekly',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false
+}
+
+export default config
