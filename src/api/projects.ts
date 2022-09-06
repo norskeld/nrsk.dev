4dc2a90a5bbf1c601902ce1f4be04d1b597f1f6b
Vladislav Mamon <norskeld@gmail.com>
2022-09-06 19:23:24 +0500

    feat: add projects section to the index page
    - Added projects section to the index page.
    - Extracted some boilerplate into `<Card />` and `<CardGrid />` components.

---
+++ src/api/projects.ts
@@ -0,0 +1,95 @@
+import markdown from 'markdown-it'
+
+export type LanguageKind = 'typescript' | 'javascript' | 'haskell' | 'rust'
+export type LanguageColor = [light: string, dark: string]
+
+export interface Language {
+  kind: LanguageKind
+  color: LanguageColor
+  name: string
+  ext: string
+}
+
+export interface Project {
+  name: string
+  description: string
+  language: Language
+  url: string
+}
+
+const languages: Array<Language> = [
+  {
+    kind: 'typescript',
+    name: 'TypeScript',
+    ext: '.ts',
+    color: ['#3178c6', '#4c83bd']
+  },
+  {
+    kind: 'rust',
+    name: 'Rust',
+    ext: '.rs',
+    color: ['#a9643d', '#dea584']
+  },
+  {
+    kind: 'haskell',
+    name: 'Haskell',
+    ext: '.hs',
+    color: ['#5e5086', '#8576ad']
+  }
+]
+
+function language(lang: LanguageKind): Language {
+  return languages.find(({ kind }) => kind === lang)!
+}
+
+async function produceProjects(): Promise<Array<Project>> {
+  const GH_BASE = 'https://github.com/norskeld'
+
+  return [
+    {
+      name: 'sigma',
+      description: 'TypeScript [parser combinator](https://en.wikipedia.org/wiki/Parser_combinator) library for building fast and convenient parsers.',
+      url: `${GH_BASE}/sigma`,
+      language: language('typescript')
+    },
+    {
+      name: 'shikigami',
+      description: 'Opinionated syntax highlighting with [shiki](https://github.com/shikijs/shiki) for [markdown-it](https://github.com/markdown-it/markdown-it).',
+      url: `${GH_BASE}/shikigami`,
+      language: language('typescript')
+    },
+    {
+      name: 'rsx',
+      description: 'Micro CLI for interactive execution of npm & yarn scripts.',
+      url: `${GH_BASE}/rsx`,
+      language: language('rust')
+    },
+    {
+      name: 'arx',
+      description: 'Simple CLI for scaffolding projects from templates in a touch.',
+      url: `${GH_BASE}/arx`,
+      language: language('rust')
+    },
+    {
+      name: 'diceware',
+      description: 'Rust crate and CLI for generating [Diceware](https://en.wikipedia.org/wiki/Diceware) passphrases.',
+      url: `${GH_BASE}/diceware`,
+      language: language('rust')
+    }
+  ]
+}
+
+export async function loadProjects(): Promise<Array<Project>> {
+  const projects = await produceProjects()
+
+  const parser = markdown('default', {
+    typographer: true
+  })
+
+  return projects.map((project) => {
+    return {
+      ...project,
+      description: parser.renderInline(project.description)
+    }
+  })
+}
