306ee24f6b1231a001b0d202e8b3fb61d396f15c
Vladislav Mamon <norskeld@gmail.com>
2023-05-22 01:19:44 +0300

    fix: fix trailing slash in paths and filter out tag pages

--- astro.config.ts
+++ astro.config.ts
@@ -4,22 +4,38 @@ import sitemap from '@astrojs/sitemap'
 import compress from 'astro-compress'
 import image from '@astrojs/image'
 
+import { unslash } from './src/api/utils'
+
 export default defineConfig({
   site: 'https://vm.codes',
-
+  trailingSlash: 'never',
   integrations: [
     image({
       serviceEntryPoint: '@astrojs/image/sharp'
     }),
+
     astrowind({
       config: {
         path: 'tailwind.config.ts',
         applyBaseStyles: false
       }
     }),
+
     sitemap({
-      lastmod: new Date()
+      lastmod: new Date(),
+
+      filter(page) {
+        return !['articles/tag'].some((pattern) => page.includes(pattern))
+      },
+
+      serialize(item) {
+        return {
+          ...item,
+          url: unslash(item.url)
+        }
+      }
     }),
+
     compress({
       css: false,
       img: true
