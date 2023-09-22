a06fba1550d05387acee8f786f6aec5631637465
Vladislav Mamon <norskeld@gmail.com>
2023-09-22 14:02:45 +0300

    refactor: simplify structure a bit by moving zod schemas to content config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,41 @@
-import { defineCollection } from 'astro:content'
+import { defineCollection, z } from 'astro:content'
 
-import articleSchema from '@/schemas/article.schema'
-import projectSchema from '@/schemas/project.schema'
+import { language } from '@/api/content/projects'
+
+function intoTimestamp(date: Date | string): number {
+  return (date instanceof Date ? date : new Date(date)).valueOf()
+}
+
+const articles = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    createdAt: z.string().or(z.date()).transform(intoTimestamp),
+    updatedAt: z.string().or(z.date()).transform(intoTimestamp).optional(),
+    draft: z.boolean().optional(),
+    tags: z.array(z.string())
+  })
+})
+
+const languages = z.union([
+  z.literal('typescript'),
+  z.literal('rust'),
+  z.literal('haskell'),
+  z.literal('ocaml')
+])
+
+const projects = defineCollection({
+  schema: z.object({
+    order: z.number().optional().default(-1),
+    name: z.string(),
+    description: z.string(),
+    url: z.string().url(),
+    language: languages.transform(language),
+    wip: z.boolean().optional().default(false)
+  })
+})
 
 export const collections = {
-  articles: defineCollection({ schema: articleSchema }),
-  projects: defineCollection({ schema: projectSchema })
+  articles,
+  projects
 }
