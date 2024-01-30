c2ed7d88c9bec5bd35e35020dc00ae8aab9c7ca2
Vladislav Mamon <norskeld@gmail.com>
2024-01-30 16:43:07 +0300

    refactor: remove unused functions

--- src/api/frontmatter.ts
+++ src/api/frontmatter.ts
@@ -3,14 +3,6 @@ export interface Tag {
   url?: string
 }
 
-export function toTags(tags: Array<string>): Array<Tag> {
-  return tags.map((tag) => ({ name: tag }))
-}
-
 export function toTagsWithURL(url: string, tags: Array<string>): Array<Tag> {
   return tags.map((tag) => ({ name: tag, url: url + tag }))
-}
-
-export function hasLinkableTags(tags: Array<Tag>): tags is Array<Required<Tag>> {
-  return tags.some(Boolean)
 }
