607a7c2ab8c362d8f336af6f8900ab23bac262be
Vladislav Mamon <norskeld@gmail.com>
2024-04-03 16:58:50 +0300

    feat: use `expressive-code`, use built-in markdown processing, refactor

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,11 @@
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import expressive from 'astro-expressive-code'
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
+import rehypeSlug from 'rehype-slug'
+import { h } from 'hastscript'
+import mdx from '@astrojs/mdx'
 import icon from 'astro-icon'
 
 import { compress } from './src/api/integrations'
@@ -10,6 +15,26 @@ export default defineConfig({
   site: 'https://vm.codes',
   trailingSlash: 'never',
   compressHTML: true,
+  markdown: {
+    remarkRehype: {
+      footnoteBackContent: '⤴'
+    },
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          content: () => h(null, '§'),
+          properties: {
+            tabIndex: -1,
+            ariaHidden: 'true',
+            class: 'heading-anchor'
+          }
+        }
+      ]
+    ]
+  },
   integrations: [
     tailwind({
       configFile: 'tailwind.config.ts',
@@ -18,19 +43,30 @@ export default defineConfig({
 
     sitemap({
       lastmod: new Date(),
+      filter: (page) => !['articles/tag'].some((pattern) => page.includes(pattern)),
+      serialize: (item) => ({ ...item, url: unslash(item.url) })
+    }),
 
-      filter(page) {
-        return !['articles/tag'].some((pattern) => page.includes(pattern))
-      },
-
-      serialize(item) {
-        return {
-          ...item,
-          url: unslash(item.url)
+    expressive({
+      themes: ['nord'],
+      styleOverrides: {
+        codeBackground: 'var(--ec-code-bg)',
+        frames: {
+          frameBoxShadowCssValue: 'var(--ec-frames-shadow)',
+          inlineButtonBorder: 'var(--ec-frames-inline-button-border)',
+          terminalBackground: 'var(--ec-code-bg)',
+          terminalTitlebarBackground: 'var(--ec-code-bg)'
+        },
+        textMarkers: {
+          markBackground: 'var(--ec-markers-bg)',
+          markBorderColor: 'var(--ec-markers-lb)',
+          delBackground: 'var(--ec-markers-del-bg)',
+          insBackground: 'var(--ec-markers-ins-bg)'
         }
       }
     }),
 
+    mdx(),
     icon(),
     compress()
   ]
