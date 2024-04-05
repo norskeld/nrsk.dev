f7f6b01a59d35a5a02867c028524508fdc9bd988
Vladislav Mamon <norskeld@gmail.com>
2024-04-05 15:22:22 +0300

    refactor: co-locate interface and inline tags mapping

--- src/api/frontmatter.ts
+++
@@ -1,8 +0,0 @@
-export interface Tag {
-  name: string
-  url?: string
-}
-
-export function toTagsWithURL(url: string, tags: Array<string>): Array<Tag> {
-  return tags.map((tag) => ({ name: tag, url: url + tag }))
-}
